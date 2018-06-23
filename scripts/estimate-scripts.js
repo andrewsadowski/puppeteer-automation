091594ee5d7e6d409706897dc6d5a3d530cfabea
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-06-22 19:27:10 -0700

    added to estimate script

--- scripts/estimate-scripts.js
+++ scripts/estimate-scripts.js
@@ -1,11 +1,13 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
 
 async function run() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.goto("https://github.com");
-  await page.screenshot({ path: "screenshots/github.png" });
+  await page.goto('https://github.com');
+  await page.screenshot({ path: 'screenshots/github.png' });
+  await page.goto('https://github.com/andrewsadowski');
+  await page.screenshot({ path: 'screenshots/github-profile.png' });
 
   browser.close();
 }
