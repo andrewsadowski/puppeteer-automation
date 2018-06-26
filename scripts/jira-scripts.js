878e658c3377c101a105c2febe706731d220d04d
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-06-25 20:57:40 -0700

    added auth object to obscure credentials

--- scripts/jira-scripts.js
+++ scripts/jira-scripts.js
@@ -1,13 +1,18 @@
 const puppeteer = require('puppeteer');
 const JiraClient = require('jira-connector');
+const jiraAuth = require('../auth.js');
 
-var jira = new JiraClient({
-  host: 'tracker.welocalize.com'
+const jira = new JiraClient({
+  host: 'tracker.welocalize.com',
+  basic_auth: {
+    username: jiraAuth.username,
+    password: jiraAuth.password
+  }
 });
 
 jira.issue.getIssue(
   {
-    issueKey: 'JWR-19'
+    issueKey: 'HCL-25'
   },
   function(error, issue) {
     console.log(issue.fields.summary);
