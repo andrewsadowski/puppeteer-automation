d6c05827d2bbf66daefe96901913f0316d24e83c
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-06-25 20:43:24 -0700

    updated jira-script with jira module and short connector script

--- scripts/jira-scripts.js
+++ scripts/jira-scripts.js
@@ -1,1 +1,15 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
+const JiraClient = require('jira-connector');
+
+var jira = new JiraClient({
+  host: 'tracker.welocalize.com'
+});
+
+jira.issue.getIssue(
+  {
+    issueKey: 'JWR-19'
+  },
+  function(error, issue) {
+    console.log(issue.fields.summary);
+  }
+);
