26493a6d2260c89221bf0eb5f5dc477b33caad10
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-06-25 21:03:04 -0700

    updated auth now fixed

--- scripts/jira-scripts.js
+++ scripts/jira-scripts.js
@@ -1,12 +1,12 @@
 const puppeteer = require('puppeteer');
 const JiraClient = require('jira-connector');
-const jiraAuth = require('../auth.js');
+const { jiraAuth1 } = require('../auth.js');
 
 const jira = new JiraClient({
   host: 'tracker.welocalize.com',
   basic_auth: {
-    username: jiraAuth.username,
-    password: jiraAuth.password
+    username: jiraAuth1.username,
+    password: jiraAuth1.password
   }
 });
 
