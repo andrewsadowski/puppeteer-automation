016a4284a2961431861e5f8e489d9374eba66236
root <andrew.n.sadowski@gmail.com>
2018-06-27 15:39:49 -0700

    added jira.search function which takes a jql filter and returns detailed defect info

--- scripts/jira-scripts.js
+++ scripts/jira-scripts.js
@@ -1,10 +1,10 @@
-const puppeteer = require('puppeteer');
-const request = require('request');
-const JiraClient = require('jira-connector');
-const { jiraAuth1 } = require('../auth.js');
+const puppeteer = require("puppeteer");
+const request = require("request");
+const JiraClient = require("jira-connector");
+const { jiraAuth1 } = require("../auth.js");
 
 const jira = new JiraClient({
-  host: 'tracker.welocalize.com',
+  host: "tracker.welocalize.com",
   basic_auth: {
     username: jiraAuth1.username,
     password: jiraAuth1.password
@@ -13,18 +13,35 @@ const jira = new JiraClient({
 
 jira.issue.getIssue(
   {
-    issueKey: 'HCL-25'
+    issueKey: "HCL-25"
   },
   function(error, issue) {
     console.log(issue.fields.summary);
   }
 );
 
-jira.attachment.getAttachment(
+jira.search.search(
   {
-    attachmentId: 'HCL-25'
+    jql:
+      'project = GOOGLE AND "Google Falcon ID" = GOOGLE_1707_11518_Multimedia_review'
   },
-  function(error, attachment) {
-    console.log(attachment);
+  (error, issue) => {
+    console.log(
+      issue.fields.issueKey,
+      issue.fields.issuetype.name,
+      issue.fields.summary,
+      issue.fields.status.name,
+      issue.fields.resolution.name,
+      issue.fields.labels
+    );
   }
 );
+
+// jira.attachment.getAttachment(
+//   {
+//     attachmentId: 'HCL-25'
+//   },
+//   function(error, attachment) {
+//     console.log(attachment);
+//   }
+// );
