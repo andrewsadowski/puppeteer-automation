7ddb02b0467d4ada7522e9a9fd87d5b28f6fa75b
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-07-15 21:47:51 -0700

    grab WC of page with page.content()

---
+++ scripts/estimate-WC.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer');
+const wordCount = require('html-word-count');
+
+async function run() {
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+  await page.goto('https://github.com');
+  const html = await page.content();
+  console.log(html);
+  let ghWC = await wordCount(html);
+  console.log(ghWC);
+  // await page.screenshot({ path: 'screenshots/github.png' });
+  // await page.goto('https://github.com/andrewsadowski');
+  // await page.screenshot({ path: 'screenshots/github-profile.png' });
+
+  browser.close();
+}
+
+run();
