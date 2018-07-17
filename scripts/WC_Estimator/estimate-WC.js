be9fb73eb752ef8e0202f976e606f238546df333
admin <andrew.n.sadowski@gmail.com>
2018-07-17 14:44:40 -0700

    cleaned up estimate-WC logging

--- scripts/WC_Estimator/estimate-WC.js
+++ scripts/WC_Estimator/estimate-WC.js
@@ -6,6 +6,23 @@ const {
   exportWCToCSV
 } = require('../helper.js');
 
+async function processWCLinks() {
+  const urls = parseUrls();
+  console.log(urls);
+  for (const url of urls) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    const html = await page.content();
+    let WC = await wordCount(html);
+    browser.close();
+    exportWCToCSV(url, WC);
+  }
+}
+
+processWCLinks();
+
+//Previous async function for getting wc
 // async function run() {
 //   const browser = await puppeteer.launch();
 
@@ -23,21 +40,3 @@ const {
 // }
 
 // run();
-
-async function processWCLinks() {
-  const urls = parseUrls();
-  console.log(urls);
-  for (const url of urls) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
-    const html = await page.content();
-    let WC = await wordCount(html);
-    console.log(WC);
-    browser.close();
-    exportWCToCSV(url, WC);
-  }
-}
-
-processWCLinks();
-// console.log(parseUrls());
