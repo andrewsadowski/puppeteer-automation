f98fea3b822026aefa8ffd24d80a00b7d4f098d8
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-07-16 17:31:23 -0700

    added processWCLinks function

--- scripts/WC_Estimator/estimate-WC.js
+++ scripts/WC_Estimator/estimate-WC.js
@@ -3,22 +3,37 @@ const wordCount = require("html-word-count");
 const fs = require("fs");
 const { parseUrls } = require("../helper.js");
 
-async function run() {
+// async function run() {
+//   const browser = await puppeteer.launch();
+
+//   const page = await browser.newPage();
+//   await page.goto("https://github.com");
+//   const html = await page.content();
+//   console.log(html);
+//   let ghWC = await wordCount(html);
+//   console.log(ghWC);
+//   // await page.screenshot({ path: 'screenshots/github.png' });
+//   // await page.goto('https://github.com/andrewsadowski');
+//   // await page.screenshot({ path: 'screenshots/github-profile.png' });
+
+//   browser.close();
+// }
+
+// run();
+
+async function processWCLinks() {
+  const urls = parseUrls();
   const browser = await puppeteer.launch();
-
   const page = await browser.newPage();
-  await page.goto("https://github.com");
-  const html = await page.content();
-  console.log(html);
-  let ghWC = await wordCount(html);
-  console.log(ghWC);
-  // await page.screenshot({ path: 'screenshots/github.png' });
-  // await page.goto('https://github.com/andrewsadowski');
-  // await page.screenshot({ path: 'screenshots/github-profile.png' });
 
+  urls.forEach(async url => {
+    await page.goto(url);
+    const html = await page.content();
+    let WC = await wordCount(html);
+    console.log(WC);
+  });
   browser.close();
 }
 
-run();
-
-console.log(parseUrls());
+processWCLinks();
+// console.log(parseUrls());
